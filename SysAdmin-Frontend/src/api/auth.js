692dd416cb03a95bcbb4c7ee42eee707d6202cae
randil-h <51442488+randil-h@users.noreply.github.com>
2025-04-21 00:05:50 +0530

    env name change

--- SysAdmin-Frontend/src/api/auth.js
+++ SysAdmin-Frontend/src/api/auth.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:6969/api/auth";
+const API_URL = import.meta.env.VITE_API_AUTH_URL || "http://localhost:6969/api/auth";
 
 axios.interceptors.request.use((config) => {
     const token = getToken();
